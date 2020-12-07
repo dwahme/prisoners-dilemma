79719128144c5700d7402c083bc74711ba20584e
Dawson Whitehead <dwahme1@gmail.com>
2020-12-07 15:59:52 -0600

    Added multigenerational support

--- runner.py
+++ runner.py
@@ -3,6 +3,23 @@ import glob
 import operator
 import os
 from subprocess import Popen, PIPE
+import uuid
+
+class Strategy:
+
+    def __init__(self, file):
+
+        self.file = file
+        self.id = uuid.uuid1()
+
+    def __eq__(self, o):
+        return self.id == o.id
+
+    def __hash__(self) -> int:
+        return self.id.__hash__()
+
+    def dup(self):
+        return Strategy(self.file)
 
 class Simulation:
     """Holds the data and runs a simulation"""
@@ -23,9 +40,9 @@ class Simulation:
         Runs a single round of a duel between two strategies
         """
 
-        strat_1_args = ["python3", self.strategy_1, 
+        strat_1_args = ["python3", self.strategy_1.file, 
                         "-m", self.hist_1, "-o", self.hist_2]
-        strat_2_args = ["python3", self.strategy_2, 
+        strat_2_args = ["python3", self.strategy_2.file, 
                         "-m", self.hist_2, "-o", self.hist_1]
 
         # Get the next set of decisions
@@ -82,18 +99,16 @@ class Simulation:
         
         return self.time_1, self.time_2
 
-def run_all(folder, rounds):
+def run_all(strats, rounds):
     """
-    Runs a full contest between all strategies in <folder> for <rounds>
+    Runs a full contest between all <strats> for <rounds>
     """
 
-    os.chdir(folder)
-    files = [f for f in glob.glob("*.py")]
     sims = []
 
-    for i, f_1 in enumerate(files):
-        for f_2 in files[i:]:
-            print(i, f_1, f_2)
+    for i, f_1 in enumerate(strats):
+        for f_2 in strats[i:]:
+            # print(i, f_1, f_2)
             sim = Simulation(f_1, f_2, rounds)
             sim.duel()
 
@@ -101,34 +116,67 @@ def run_all(folder, rounds):
 
     return sims
 
-def dump_results(sims):
+def get_totals(sims):
+
+    res = {}
+
+    for s in sims:
+        s_1 = s.strategy_1
+        s_2 = s.strategy_2
+
+        res[s_1] = s.time_1 + res.get(s_1, 0)
+        res[s_2] = s.time_2 + res.get(s_2, 0)
+
+    return sorted(res.items(), key=operator.itemgetter(1))
+
+def run_generations(folder, rounds, generations):
+    """
+    Runs <generations> contests, weeding out the lowest-performing strategies
+    """
+
+    os.chdir(folder)
+    files = [f for f in glob.glob("*.py")]
+    strats = [Strategy(f) for f in files]
+
+    to_kill = len(files) // 3
+
+    for i in range(generations):
+        sims = run_all(strats, rounds)
+        totals = get_totals(sims)
+        dump_results(sims, totals, i)
+
+        grow = [s for s, _ in totals[:to_kill]]
+        same = [s for s, _ in totals[to_kill:-to_kill]]
+        strats = grow + same + [s.dup() for s in grow]
+
+
+def dump_results(sims, totals, gen):
     """"
     Finds the winner for the contest
     Prints out the results
     """
 
-    res = {}
-
-    print("Individual Results\n---------------")
+    print(f"GENERATION {gen}")
+    print("  Individual Results---------------")
 
     for s in sims:
-        s_1 = s.strategy_1.split(".")[0]
-        s_2 = s.strategy_2.split(".")[0]
+        s_1 = s.strategy_1.file.split(".")[0]
+        s_2 = s.strategy_2.file.split(".")[0]
 
-        print(f"{s_1} vs {s_2}: {s.time_1}-{s.time_2}")
-        res[s_1] = s.time_1 + res.get(s_1, 0)
-        res[s_2] = s.time_1 + res.get(s_2, 0)
+        print(f"    {s_1} vs {s_2}: {s.time_1}-{s.time_2}")
 
-    print("\nOverall Scores\n----------------")
+    print("  Overall Scores----------------")
 
-    for k, v in sorted(res.items(), key=operator.itemgetter(1)):
-        print(f"{k}: {v}")
+    for k, v in totals:
+        print(f"    {k.file}: {v}")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Runs the prisoner\'s dilemma contest')
     parser.add_argument('iterations', metavar='n', type=int,
                         help='How many times to run each simulation')
+    parser.add_argument('-g', type=int, dest="generations", default=1,
+                        help='The number of generations')
     parser.add_argument('-a', type=str, dest="strat_1", default="",
                         help='The first strategy')
     parser.add_argument('-b', type=str, dest="strat_2", default="",
@@ -143,5 +191,4 @@ if __name__ == "__main__":
         print(sim.duel(), sim.calc_time())
 
     elif args.folder != "":
-        sims = run_all(args.folder, args.iterations)
-        dump_results(sims)
+        run_generations(args.folder, args.iterations, args.generations)
